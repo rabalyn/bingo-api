a77f6faae8404951c96a9f918dbd6246862426b0
nm94cecu <nils.mundhenke@tu-darmstadt.de>
2021-05-22 13:32:27 +0200

    remove cluster mode from pm2, need more testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: 'bingo',
       script: './src/index.js',
       instance_var: 'INSTANCE_ID',
-      instances: 4,
-      exec_mode: 'cluster',
+      // instances: 4,
+      // exec_mode: 'cluster',
       watch: true,
       env: {
         NODE_ENV: 'development',
